d48d326fa3f00f195cee2cb932ad9be6a55feffd
pavlo <pavel@healthdom.com>
2022-04-01 19:20:52 +0300

    Synerise ios initialization

--- ios/Classes/SwiftSyneriseFlutterPlugin.swift
+++ ios/Classes/SwiftSyneriseFlutterPlugin.swift
@@ -2,7 +2,7 @@ import Flutter
 import UIKit
 import SyneriseSDK
 
-public class SwiftSyneriseFlutterPlugin: NSObject, FlutterPlugin {
+public class SwiftSyneriseFlutterPlugin: NSObject, FlutterPlugin, SyneriseDelegate {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "synerise_flutter", binaryMessenger: registrar.messenger())
     let instance = SwiftSyneriseFlutterPlugin()
@@ -10,6 +10,22 @@ public class SwiftSyneriseFlutterPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+      if (call.method == "initSynerise") {
+          guard let args = call.arguments as? [String: String] else {
+              result("iOS could not recognize flutter arguments for method initSynerise()")
+              return
+          }
+          initSynerise(apiKey: args["apiKey"]!, appId: args["appId"]!)
+          result("Synerise client UUID: " + Client.getUUID())
+      }
+      result("Method not implemented")
   }
+    
+    private func initSynerise(apiKey: String, appId: String) {
+        Synerise.initialize(clientApiKey: apiKey)
+        Synerise.setDebugModeEnabled(true)
+        Synerise.setCrashHandlingEnabled(true)
+        Synerise.setDelegate(self)
+    }
+    
 }
